80db61e8f2fb3a745658dcfef8f7cf55eca09b0e
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-11-03 19:30:43 -0300

    Requisição total

--- requisicoes/src/App.jsx
+++ requisicoes/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Itens } from "./components/Itens";
 import "./App.css";
 
 function App() {
@@ -8,12 +9,28 @@ function App() {
   useEffect(() => {
     const fetchAPI = fetch(URL)
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => setNutri(data))
+      .catch((erro) => console.log("Erro na requisição: " + erro));
   }, []);
 
   return (
     <>
       <h1>Requisições HTPPS com React</h1>
+
+      <div>
+        <h2>Receitas Nutris</h2>
+        {nutri && (
+          <ul style={{ listStyleType: "none" }}>
+            {nutri.map((item, index) => (
+              <Itens image={item.capa} key={item.id}>
+                <h3>{item.titulo}</h3>
+                <h4>{item.subtitulo}</h4>
+                <p>{item.categoria}</p>
+              </Itens>
+            ))}
+          </ul>
+        )}
+      </div>
     </>
   );
 }
