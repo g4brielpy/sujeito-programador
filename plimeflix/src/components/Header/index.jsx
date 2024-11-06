9bc5ed3ca94b69909b9ba9b0232843d29df5574a
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-11-05 21:57:17 -0300

    Configurando container

--- plimeflix/src/components/Header/index.jsx
+++ plimeflix/src/components/Header/index.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="bg-black text-white">
+      <div className="container py-4 flex justify-around">
+        <Link to={"/"}>Plime Flix</Link>
+        <Link to={"/favoritos"}>Meus Filmes</Link>
+      </div>
     </header>
   );
 }
