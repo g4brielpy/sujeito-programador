a29b94de930dc49f357d4abe3c42c41ad92ac834
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-11-06 20:22:19 -0300

    Adicionar estilos responsivos

--- plimeflix/src/components/Header/index.jsx
+++ plimeflix/src/components/Header/index.jsx
@@ -4,8 +4,14 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="bg-black text-white">
-      <div className="container py-4 flex justify-around">
-        <Link to={"/"}>Plime Flix</Link>
+      <div
+        className="
+        container py-4 flex justify-around 
+        md:text-xl lg:text-2xl"
+      >
+        <Link to={"/"} className="font-bold">
+          Plime Flix
+        </Link>
         <Link to={"/favoritos"}>Meus Filmes</Link>
       </div>
     </header>
