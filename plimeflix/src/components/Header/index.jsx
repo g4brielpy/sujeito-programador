07904936e4a873c7d71e703ee1b1c5062288efc1
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-11-09 18:19:09 -0300

    Adicionar componente Button

--- plimeflix/src/components/Header/index.jsx
+++ plimeflix/src/components/Header/index.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { ButtonLink } from "../IU/Button";
 
 export default function Header() {
   return (
     <header className="bg-black text-white">
-      <div
+      <nav
         className="
-        container py-4 flex justify-around 
+        container py-4 flex justify-around items-center
         md:text-xl lg:text-2xl"
       >
         <Link to={"/"} className="font-bold">
           Plime Flix
         </Link>
-        <Link to={"/favoritos"}>Meus Filmes</Link>
-      </div>
+        <ButtonLink href={"/favoritos"}>Meus Filmes</ButtonLink>
+      </nav>
     </header>
   );
 }
