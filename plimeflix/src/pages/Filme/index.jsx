c874838a90d3648865fc3157e50b3773396d998c
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-11-09 18:33:57 -0300

    Adicionando estado de loading

--- plimeflix/src/pages/Filme/index.jsx
+++ plimeflix/src/pages/Filme/index.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import { ButtonLink, ButtonA } from "../../components/IU/Button";
 import { API } from "../../utils/api";
 
+import Loading from "./loading";
+
 export default function Filme() {
   const { id } = useParams();
 
@@ -29,32 +31,38 @@ export default function Filme() {
     fetchFilme();
   }, []);
 
-  return (
-    <article className="container py-16 flex justify-center">
-      <div className=" bg-black text-white w-full max-w-[600px] p-4 rounded-sm">
-        <h1
-          className="
+  if (loading) {
+    return <Loading />;
+  } else {
+    return (
+      <article className="container py-16 flex justify-center">
+        <div className=" bg-black text-white w-full max-w-[600px] p-4 rounded-sm">
+          <h1
+            className="
           font-bold text-center
           text-2xl md:text-3xl lg:text-4xl"
-        >
-          {filme.title}
-        </h1>
-        <img
-          className="object-cover m-auto my-10 rounded-md"
-          src={`https://image.tmdb.org/t/p/w500/${filme.poster_path}`}
-          alt={filme.original_title}
-        />
-        <div>
-          <h2 className="text-xl md:text-2xl lg:text-3xl mb-6 font-bold">
-            Sinopse
-          </h2>
-          <p className="md:text-lg lg:text-xl text-justify">{filme.overview}</p>
+          >
+            {filme.title}
+          </h1>
+          <img
+            className="object-cover m-auto my-10 rounded-md"
+            src={`https://image.tmdb.org/t/p/w500/${filme.poster_path}`}
+            alt={filme.original_title}
+          />
+          <div>
+            <h2 className="text-xl md:text-2xl lg:text-3xl mb-6 font-bold">
+              Sinopse
+            </h2>
+            <p className="md:text-lg lg:text-xl text-justify">
+              {filme.overview}
+            </p>
+          </div>
+          <nav className="mt-12 flex justify-around flex-wrap">
+            <ButtonLink href={"/"}>Voltar</ButtonLink>
+            <ButtonA href={"https"}>Ver treile</ButtonA>
+          </nav>
         </div>
-        <nav className="mt-12 flex justify-around flex-wrap">
-          <ButtonLink href={"/"}>Voltar</ButtonLink>
-          <ButtonA href={"https"}>Ver treile</ButtonA>
-        </nav>
-      </div>
-    </article>
-  );
+      </article>
+    );
+  }
 }
---
+++ plimeflix/src/pages/Filme/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <>
+      <section className="container py-16 flex items-center justify-center ">
+        <div className="h-[1000px] max-w-full w-[600px] rounded-md shadow-sm bg-black"></div>
+      </section>
+    </>
+  );
+}
