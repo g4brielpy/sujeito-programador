12c2933e7f227a50d7a0b76c2f1ca9df6ed9f35f
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-11-11 22:18:45 -0300

    Buscando filmes do localStorage

--- plimeflix/src/pages/Favoritos/index.jsx
+++ plimeflix/src/pages/Favoritos/index.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 export default function Favoritos() {
+  const [filmesFavoritos, setFilmesFavoritos] = useState([]);
+
+  useEffect(() => {
+    const filmesStorage = JSON.parse(localStorage.getItem("@filmesFavoritos"));
+
+    if (filmesStorage) {
+      setFilmesFavoritos(filmesFavoritos);
+    }
+  }, []);
+
   return (
     <>
-      <h1>Meus Favoritos</h1>
+      <h1
+        className="
+          font-bold text-center mb-8
+          text-2xl md:text-3xl lg:text-4xl"
+      >
+        Meus Favoritos
+      </h1>
+      <section></section>
     </>
   );
 }
