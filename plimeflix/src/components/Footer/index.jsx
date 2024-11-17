9cfff8b09e0533332ea77c3496bac20a9f2edcb5
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-11-17 20:05:58 -0300

    Construindo o footer

--- plimeflix/src/components/Footer/index.jsx
+++ plimeflix/src/components/Footer/index.jsx
@@ -2,8 +2,19 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className="bg-black text-white text-center p-4">
+      <p>
+        &copy; Desenvolvido por{" "}
+        <a
+          href="http://"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-bold hover:underline"
+        >
+          Gabriel Iuri{" "}
+        </a>
+        - 2024
+      </p>
     </footer>
   );
 }
