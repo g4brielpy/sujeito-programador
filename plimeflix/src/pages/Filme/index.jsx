89974dc433350ab5b1e96f6a12b67295d98d798e
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-11-17 19:54:53 -0300

    Correção de código

--- plimeflix/src/pages/Filme/index.jsx
+++ plimeflix/src/pages/Filme/index.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { ButtonLink, ButtonA } from "../../components/IU/Button";
+
+import { ButtonA } from "../../components/IU/Button";
 import { API } from "../../utils/api";
 
 import Loading from "./loading";
@@ -9,7 +10,9 @@ export default function Filme() {
   const { id } = useParams();
 
   const [filme, setFilme] = useState({});
+  const [filmesStorage, setFilmesStorage] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [isSalvo, setIsSalvo] = useState(false);
 
   useEffect(() => {
     async function fetchFilme() {
@@ -22,65 +25,81 @@ export default function Filme() {
         });
         setFilme(responde.data);
       } catch (e) {
-        console.log("Error: " + e);
+        console.error("Error: ", e);
       } finally {
         setLoading(false);
       }
     }
 
-    fetchFilme();
-  }, []);
-
-  function saveMovie(id) {
-    const filmesStorage =
+    // Recuperar filmes salvos e verificar se o filme atual já está salvo
+    const filmesSalvos =
       JSON.parse(localStorage.getItem("@filmesFavoritos")) || [];
-    filmesStorage.push(filme);
+    setFilmesStorage(filmesSalvos);
 
-    localStorage.setItem("@filmesFavoritos", JSON.stringify(filmesStorage));
+    if (filmesSalvos.find((filme) => filme.id === parseInt(id, 10))) {
+      setIsSalvo(true);
+    }
+
+    fetchFilme();
+  }, [id]);
+
+  function saveMovie() {
+    // Verificar se o filme já está favoritado
+    if (isSalvo) {
+      alert("Filme já favoritado");
+      return;
+    }
+
+    // Atualizar o estado com o novo filme salvo
+    const novosFilmes = [...filmesStorage, filme];
+    setFilmesStorage(novosFilmes);
+
+    // Atualizar o localStorage
+    localStorage.setItem("@filmesFavoritos", JSON.stringify(novosFilmes));
+
+    alert("Filme salvo com sucesso!");
+    setIsSalvo(true); // Desativa o botão
   }
 
   if (loading) {
     return <Loading />;
-  } else {
-    return (
-      <article className="container py-16 flex justify-center">
-        <div className=" bg-black text-white w-full max-w-[600px] p-4 rounded-sm">
-          <h1
-            className="
-          font-bold text-center
-          text-2xl md:text-3xl lg:text-4xl"
-          >
-            {filme.title}
-          </h1>
-          <img
-            className="object-cover m-auto my-10 rounded-md"
-            src={`https://image.tmdb.org/t/p/w500/${filme.poster_path}`}
-            alt={filme.original_title}
-          />
-          <div>
-            <h2 className="text-xl md:text-2xl lg:text-3xl mb-6 font-bold">
-              Sinopse
-            </h2>
-            <p className="md:text-lg lg:text-xl text-justify">
-              {filme.overview}
-            </p>
-          </div>
-          <nav className="mt-12 flex justify-around flex-wrap">
-            <button
-              onClick={() => saveMovie(id)}
-              className="px-6 py-2 bg-red-900 text-white rounded-sm"
-            >
-              Salvar
-            </button>
-            <ButtonA
-              target="_blank"
-              href={`https://www.youtube.com/results?search_query=${filme.title}`}
-            >
-              Ver Trailer
-            </ButtonA>
-          </nav>
-        </div>
-      </article>
-    );
   }
+
+  return (
+    <article className="container py-16 flex justify-center">
+      <div className="bg-black text-white w-full max-w-[600px] p-4 rounded-sm">
+        <h1 className="font-bold text-center text-2xl md:text-3xl lg:text-4xl">
+          {filme.title}
+        </h1>
+        <img
+          className="object-cover m-auto my-10 rounded-md"
+          src={`https://image.tmdb.org/t/p/w500/${filme.poster_path}`}
+          alt={filme.original_title}
+        />
+        <div>
+          <h2 className="text-xl md:text-2xl lg:text-3xl mb-6 font-bold">
+            Sinopse
+          </h2>
+          <p className="md:text-lg lg:text-xl text-justify">{filme.overview}</p>
+        </div>
+        <nav className="mt-12 flex justify-around flex-wrap">
+          <button
+            onClick={saveMovie}
+            className={`px-6 py-2 bg-red-900 text-white rounded-sm ${
+              isSalvo ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+            disabled={isSalvo}
+          >
+            {isSalvo ? "Salvo" : "Salvar"}
+          </button>
+          <ButtonA
+            target="_blank"
+            href={`https://www.youtube.com/results?search_query=${filme.title}`}
+          >
+            Ver Trailer
+          </ButtonA>
+        </nav>
+      </div>
+    </article>
+  );
 }
