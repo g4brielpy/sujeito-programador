0bbcc47ed5c98ac351e56eb087474323f9cad4cd
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-11-22 06:26:44 -0300

    Criando formulário com os dados

--- firebase/app/src/App.jsx
+++ firebase/app/src/App.jsx
@@ -1,12 +1,48 @@
 import { useState } from "react";
+import { db } from "./utils/firebaseConnection";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [titulo, setTitulo] = useState("");
+  const [autor, setAutor] = useState("");
 
   return (
     <>
       <h1>Utilizando Firebase Database</h1>
+
+      <form action="#">
+        <div className="box-input">
+          <label htmlFor="titulo">Titulo: </label>
+          <input
+            type="text"
+            placeholder="Titulo do post"
+            id="titulo"
+            value={titulo}
+            onChange={(event) => setTitulo(event.target.value)}
+          />
+        </div>
+        <div className="box-input">
+          <label htmlFor="autor">Autor: </label>
+          <input
+            type="text"
+            placeholder="Autor do post"
+            id="autor"
+            value={autor}
+            onChange={(event) => setAutor(event.target.value)}
+          />
+        </div>
+        <div className="container-buttons">
+          <button
+            onClick={(event) => {
+              event.preventDefault();
+              console.log("Titulo: " + titulo + " - Autor: " + autor);
+            }}
+          >
+            Cadastrar post
+          </button>
+          <button>Buscas posts</button>
+        </div>
+      </form>
     </>
   );
 }
