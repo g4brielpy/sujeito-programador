702eaf02f5c0c4760c598891ad566bdf8dd15ccb
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-12-03 22:06:50 -0300

    Adicionando auth do firebase

--- firebase/app/src/utils/firebaseConnection.js
+++ firebase/app/src/utils/firebaseConnection.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -16,4 +17,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+
+export const auth = getAuth(firebaseApp);
 export const db = getFirestore(firebaseApp);
