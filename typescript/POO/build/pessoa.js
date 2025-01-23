a828f7b1370c28ffd959bd476ba82c8ea4e16c76
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-01-23 07:34:39 -0300

    Definindo uma class

--- typescript/POO/build/pessoa.js
+++ typescript/POO/build/pessoa.js
@@ -1,1 +1,12 @@
 "use strict";
+class Pessoa {
+    constructor(nome, idade, endereco) {
+        // inicializando a instância com os atributos definidos.
+        this.nome = nome;
+        this.idade = idade;
+        this.endereco = endereco;
+        // incrementando o atributo da class a cada instância criada.
+        Pessoa.totalPessoas++;
+    }
+}
+const pessoa = new Pessoa("Gabriel", 19, "Belo Horizonte");
--- typescript/POO/src/pessoa.ts
+++ typescript/POO/src/pessoa.ts
@@ -1,1 +1,21 @@
+class Pessoa {
+  // Atributos pertencente de cada Instância.
+  nome: string;
+  idade: number;
+  endereco: string;
 
+  // atributo pertencente a Class.
+  static totalPessoas: number;
+
+  constructor(nome: string, idade: number, endereco: string) {
+    // inicializando a instância com os atributos definidos.
+    this.nome = nome;
+    this.idade = idade;
+    this.endereco = endereco;
+
+    // incrementando o atributo da class a cada instância criada.
+    Pessoa.totalPessoas++;
+  }
+}
+
+const pessoa: Pessoa = new Pessoa("Gabriel", 19, "Belo Horizonte");
