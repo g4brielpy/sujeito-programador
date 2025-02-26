1e7e04415530644dcbc33bdce5ddcb15d664013e
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-02-26 18:03:09 -0300

    Crianção do formulário

--- to_do_list/src/App.tsx
+++ to_do_list/src/App.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [inputTarefa, setInputTarefa] = useState<string>("");
+
+  return (
+    <>
+      <h1>Lista de Tarefas</h1>
+
+      <form className="form">
+        <input
+          type="text"
+          placeholder="Digite uma nova tarefa"
+          onChange={(e) => setInputTarefa(e.target.value)}
+        />
+        <button type="submit">Adicionar</button>
+      </form>
+    </>
+  );
 }
 
 export default App;
