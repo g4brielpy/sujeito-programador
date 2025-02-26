3f21d92efdc6c121e61b721a60d27e6f738c86c6
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-02-26 18:33:56 -0300

    Criando lista de tarefas

--- to_do_list/src/App.tsx
+++ to_do_list/src/App.tsx
@@ -3,12 +3,28 @@ import "./App.css";
 
 function App() {
   const [inputTarefa, setInputTarefa] = useState<string>("");
+  const [tarefas, setTarefas] = useState<string[]>([]);
+
+  function adicinarTarefa(): void {
+    if (inputTarefa == "") {
+      return;
+    }
+
+    setTarefas((prevTarefas) => [...prevTarefas, inputTarefa]);
+    setInputTarefa("");
+  }
 
   return (
     <>
       <h1>Lista de Tarefas</h1>
 
-      <form className="form">
+      <form
+        className="form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          adicinarTarefa();
+        }}
+      >
         <input
           type="text"
           placeholder="Digite uma nova tarefa"
@@ -16,6 +32,16 @@ function App() {
         />
         <button type="submit">Adicionar</button>
       </form>
+
+      {tarefas.length > 0 && (
+        <section>
+          <ul className="lista-tarefas">
+            {tarefas.map((tarefa, index) => (
+              <li key={index}>{tarefa}</li>
+            ))}
+          </ul>
+        </section>
+      )}
     </>
   );
 }
