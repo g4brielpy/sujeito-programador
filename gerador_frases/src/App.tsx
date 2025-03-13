b55b69d094e227532a3a09a90885c91fdf6d032e
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-03-13 07:55:37 -0300

    Ajuste da logo

--- gerador_frases/src/App.tsx
+++ gerador_frases/src/App.tsx
@@ -8,15 +8,15 @@ function App() {
     <>
       <main className="main-principal">
         <div className="max-conteudo">
-          <img src="/src/assets/logo.png" alt="Dev Frases" />
+          <img className="logo" src="/src/assets/logo.png" alt="Dev Frases" />
           <section className="section-categoria">
             <h2>Categorias</h2>
             <div>
               <ButtonCategoria>Motivação</ButtonCategoria>
               <ButtonCategoria>Bom Dia</ButtonCategoria>
             </div>
+            <ButtonFrase>Gerar Frase</ButtonFrase>
           </section>
-          <ButtonFrase>Gerar Frase</ButtonFrase>
         </div>
       </main>
     </>
