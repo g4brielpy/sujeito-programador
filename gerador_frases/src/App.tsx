4366cf29dac33474dbd0ad27ae3c42c065c7becb
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-03-13 07:32:51 -0300

    Criando seção categoria

--- gerador_frases/src/App.tsx
+++ gerador_frases/src/App.tsx
@@ -5,16 +5,17 @@ import { ButtonCategoria } from "./components/ButtonCategoria";
 function App() {
   return (
     <>
-      <main>
-        <img src="/src/assets/logo.png" alt="Dev Frases" />
-        <section>
-          <h2>Categorias</h2>
-
-          <div>
-            <ButtonCategoria>Motivação</ButtonCategoria>
-            <ButtonCategoria>Bom Dia</ButtonCategoria>
-          </div>
-        </section>
+      <main className="main-principal">
+        <div className="max-conteudo">
+          <img src="/src/assets/logo.png" alt="Dev Frases" />
+          <section className="section-categoria">
+            <h2>Categorias</h2>
+            <div>
+              <ButtonCategoria>Motivação</ButtonCategoria>
+              <ButtonCategoria>Bom Dia</ButtonCategoria>
+            </div>
+          </section>
+        </div>
       </main>
     </>
   );
