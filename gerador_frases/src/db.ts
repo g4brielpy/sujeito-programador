0922d3b2ebe7d966feb5866ee091c08f88383ef7
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-03-19 20:53:00 -0300

    Adicinando frases do projeto

---
+++ gerador_frases/src/db.ts
@@ -0,0 +1,41 @@
+interface frasesProps {
+  id: number;
+  nome: string;
+  frases: string[];
+}
+
+export const frases: frasesProps[] = [
+  {
+    id: 0,
+    nome: "Motivação ",
+    frases: [
+      "Persistência é o caminho do êxito.",
+      "Grandes conquistas começam com pequenos passos.",
+      "Acredite, você é capaz de alcançar seus objetivos.",
+      "O sucesso é a soma de pequenos esforços repetidos dia após dia.",
+      "Nunca é tarde demais para ser o que você poderia ter sido.",
+      "Quanto maior a dificuldade, maior a glória.",
+      "A determinação hoje leva ao sucesso amanhã.",
+      "Você não falha até que desista de tentar.",
+      "Encare cada desafio como uma oportunidade de crescimento.",
+      "O segredo do sucesso é a constância no objetivo.",
+    ],
+  },
+
+  {
+    id: 1,
+    nome: "Bom dia",
+    frases: [
+      "Bom dia! Que seu dia seja leve e cheio de boas energias!",
+      "Acorde com gratidão e seu dia será maravilhoso!",
+      "Que hoje seja um dia de conquistas e sorrisos. Bom dia!",
+      "Bom dia! Lembre-se: cada novo dia é uma nova chance para ser feliz.",
+      "Que o sol ilumine seu caminho e traga muitas alegrias. Bom dia!",
+      "Sorria! Um novo dia começou e com ele novas oportunidades.",
+      "Que seu dia seja tão incrível quanto você. Bom dia!",
+      "A vida é feita de momentos. Aproveite cada um deles. Bom dia!",
+      "Bom dia! Que hoje seja repleto de boas notícias e motivos para sorrir!",
+      "Comece o dia com fé e otimismo, e tudo fluirá da melhor forma!",
+    ],
+  },
+];
