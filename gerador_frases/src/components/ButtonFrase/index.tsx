55300490d30ca5dd4e14257a29e539e37482aacf
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-03-19 21:36:29 -0300

    Extendendo os atributos do button frase

--- gerador_frases/src/components/ButtonFrase/index.tsx
+++ gerador_frases/src/components/ButtonFrase/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./index.css";
 
-interface ButtonFraseProps {
+interface ButtonFraseProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export function ButtonFrase({ children }: ButtonFraseProps) {
-  return <button className="button-frase">{children}</button>;
+export function ButtonFrase({ children, ...rest }: ButtonFraseProps) {
+  return (
+    <button className="button-frase" {...rest}>
+      {children}
+    </button>
+  );
 }
