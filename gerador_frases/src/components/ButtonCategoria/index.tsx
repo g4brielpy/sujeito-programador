b99563e32994d253473f6f8fecc4ac45ba75ea71
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-03-19 21:01:32 -0300

    Herdando todas as propriedades de um button

--- gerador_frases/src/components/ButtonCategoria/index.tsx
+++ gerador_frases/src/components/ButtonCategoria/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./index.css";
 
-interface ButtonCategoriaProps {
+interface ButtonCategoriaProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export function ButtonCategoria({ children }: ButtonCategoriaProps) {
-  return <button className="button-categoria">{children}</button>;
+export function ButtonCategoria({ children, ...rest }: ButtonCategoriaProps) {
+  return (
+    <button className="button-categoria" {...rest}>
+      {children}
+    </button>
+  );
 }
