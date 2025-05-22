2a55b5a28e863e158c39aa5614dfeddc2340a1ea
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-05-22 07:54:16 -0300

    Consumidos valores do AuthContext

--- context/src/App.tsx
+++ context/src/App.tsx
@@ -1,9 +1,63 @@
 import "./App.css";
 
+import { useContext, useState } from "react";
+import { AuthContext } from "./contexts/AuthContext";
+
 function App() {
+  const [inputUser, setInputUser] = useState<string>("");
+
+  const auth = useContext(AuthContext);
+  if (!auth) {
+    return <div>Loading...</div>;
+  }
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (inputUser.trim() === "") {
+      alert("Digite um nome válido");
+      return;
+    }
+
+    auth.login(inputUser);
+    setInputUser("");
+  };
+
   return (
     <>
-      <h1>Aprendendo context API</h1>
+      <h1>Context User</h1>
+      <br />
+      <h2>Olá {auth?.user || "Visitante"}</h2>
+      <form
+        onSubmit={handleLogin}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          border: "1px solid black",
+          padding: "10px",
+          borderRadius: "5px",
+        }}
+      >
+        <h3>Faça Login</h3>
+        <input
+          type="text"
+          placeholder="Digite seu nome"
+          value={inputUser}
+          onChange={(e) => setInputUser(e.target.value)}
+          style={{
+            padding: "12px 10px",
+            borderRadius: "5px",
+            border: "1px solid black",
+            fontSize: "16px",
+          }}
+        />
+
+        <button>Login</button>
+        <button type="button" onClick={auth.logout}>
+          Deslogar
+        </button>
+      </form>
     </>
   );
 }
