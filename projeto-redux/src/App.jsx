259190f07ce674a4cb44a15a69d5a649334b1503
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-07-27 13:16:23 -0300

    Instalação do redux

--- projeto-redux/src/App.jsx
+++ projeto-redux/src/App.jsx
@@ -1,21 +1,21 @@
-import { createBrowserRouter } from 'react-router-dom'
-import { Home } from './pages/Home'
-import { Login } from './pages/Login'
-import { Address } from './pages/address'
- 
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Address } from "./pages/address";
+
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Login/>
+    path: "/",
+    element: <Login />,
   },
   {
-    path: '/painel',
-    element: <Home/>
+    path: "/painel",
+    element: <Home />,
   },
   {
-    path: '/address',
-    element: <Address/>
+    path: "/address",
+    element: <Address />,
   },
-])
+]);
 
 export { router };
